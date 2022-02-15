09df35779f23734e359526db9ccc605c16aab992
Olukeye <seunolukeye2000@gmail.com>
2022-02-15 21:13:21 +0100

    fixes

--- middleware/uploads.ts
+++ middleware/uploads.ts
@@ -1,4 +1,5 @@
 import utils from 'util';
+import { Request } from 'express';
 import multer from 'multer';
 import { customAlphabet } from 'nanoid'
 import path from 'path';
@@ -10,11 +11,11 @@ export const nanoid = customAlphabet("abcdefghijjlmnopqrstuvwxyz", 10)
 
 
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, __dirname + '/resources/static/assets/uploads/');
+    destination: (req:Request, file, cb) => {
+        cb(null, __basedir + '/resources/static/assets/uploads/');
     },
     filename:(req, File, cb) => {
-        const ext = path.extname(file.originalname)
+        const ext = path.extname(File.originalname)
         const fileName =`post_${nanoid()}${Date.now()}${ext}`
         cb(null, fileName);
     }
@@ -23,8 +24,8 @@ let storage = multer.diskStorage({
 let uploadFile = multer({
     storage: storage,
     limits: { fileSize: maxSize },
-    fileFilter: filterPost
+    fileFilter: postFilter,
 }).single('file');
 
-let uploadFileMiddleware = util.promisify(uploadFile);
+let uploadFileMiddleware = utils.promisify(uploadFile);
 export default uploadFileMiddleware;
