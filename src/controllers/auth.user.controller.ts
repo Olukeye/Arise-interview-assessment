82e449a12e367ccaaa28fefdf04a2e2ff732c32e
Olukeye <seunolukeye2000@gmail.com>
2022-03-01 22:38:46 +0100

    Auth Refactor

--- src/controllers/auth.user.controller.ts
+++ src/controllers/auth.user.controller.ts
@@ -5,6 +5,13 @@ import {QueryResult} from 'pg';
 export const Register = async(req:Request, res:Response): Promise<Response>=> {
     try{
       const {email, password, full_name} = req.body;
+      // check for exixting user
+        pool.query('SELECT p FROM person  WHERE p.email = $1',[email], (results) => {
+         if(results.rows.length) {
+           return res.send("User Already Exist!")
+         }
+      })
+
       const response: QueryResult = await pool.query('INSERT INTO person (email, password, full_name) VALUES ($1, $2, $3)', [email, password, full_name]);
       return res.status(201).json({msg: "User created successfully!!"})
     } catch(e) {
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,14 +1,13 @@
 import {Router} from 'express';
 const router = Router();
 
+
 import {getUsers, getUserById, deleteUser, updateUser} from '../controllers/user.controller';
 
-router.get('/users', getUsers);
-router.get('/user/:id', getUserById);
-router.put('/user/:id', updateUser)
-router.delete('/user/:id', deleteUser);
-
-
+router.get('/users',getUsers);
+router.get('/user/:id',getUserById);
+router.put('/user/:id',updateUser)
+router.delete('/user/:id',deleteUser);
 
 
 
