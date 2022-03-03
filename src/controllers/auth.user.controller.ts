49a73f14ad727261c5af2539135a536d22288588
Olukeye <seunolukeye2000@gmail.com>
2022-03-03 23:59:33 +0100

    auth fixes...

--- src/controllers/auth.user.controller.ts
+++ src/controllers/auth.user.controller.ts
@@ -6,7 +6,12 @@ export const Register = async(req:Request, res:Response): Promise<Response>=> {
     try{
       const {email, password, full_name} = req.body;
       // check for exixting user
-    
+        pool.query('SELECT * FROM person WHERE email = $1',[email], (results) => {
+         if(results.rows.length > 0) {
+           return res.send("User Already Exist!")
+         }
+      })
+
       const response: QueryResult = await pool.query('INSERT INTO person (email, password, full_name) VALUES ($1, $2, $3)', [email, password, full_name]);
       return res.status(201).json({msg: "User created successfully!!"})
     } catch(e) {
